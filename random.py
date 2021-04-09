8bfa665596108c8136a58d92eabb5a8fb46d3ed1
Arnulf Krokeide <arnulf.krokeide@gmail.com>
2021-04-09 17:25:09 +0200

    Update random.py

--- random.py
+++ random.py
@@ -1,10 +1,2 @@
-# Python3 program to demonstrate
-# the use of sample() function .
-  
-# import random 
-from random import sample
-  
-# Prints list of random items of given length
-list1 = [1, 2, 3, 4, 5] 
-  
-print(sample(list1,3))
+import shutil
+shutil.copytree('SurveySourceForPython', 'SurveySourceForPython.Out')
